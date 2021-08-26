bdb2c117c57e4a92907a8315ef49547028fad52b
kimi <kimi0230@gmail.com>
2021-08-26 17:24:46 +0800

    gorm

--- app/models/User.go
+++ app/models/User.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type User struct {
 	BaseModel
 	Account  string `json:"account"`
@@ -9,6 +11,23 @@ type User struct {
 
 func (User) TableName() string {
 	return "users"
+}
+
+func (u *User) GetUserByField(field, value string) (*User, error) {
+	var user *User
+	if err := db.Model(user).Where(fmt.Sprintf("%v = ?", field), value).First(user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (u *User) GetUserByID(id int) (*User, error) {
+	user := &User{}
+	if err := db.Model(user).Where("id = ?", id).First(user).Error; err != nil {
+		fmt.Println("kkkkk")
+		return user, err
+	}
+	return user, nil
 }
 
 func (u *User) Get() (*User, error) {
